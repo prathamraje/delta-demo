9c4c9017adb8b1e4224991c6716f2381fb6aeadf
Pratham Raje <pratham.raje20@gmail.com>
2025-07-04 17:15:58 +0530

    updated and new file

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # delta-demo
 these is demo for git and github
+ # my new update
+ it is update to shoe on github
+ # CSK 
+ it is team lead by ms dhoni
---
+++ my git.java
@@ -0,0 +1,9 @@
+
+
+public class mygit {
+    public static void main(String[] args) {
+          System.out.println("hello GIT");
+    }
+    System.out.println("hello GIT");
+    
+}
